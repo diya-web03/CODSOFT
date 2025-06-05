530a24a3e0d60bcb98fd291d412bc3b5390c2d9a
Diya Mevada <diyamevada953@gmail.com>
2025-06-05 16:21:08 +0530

    Update task 3.py

---
+++ task 3.py
@@ -0,0 +1,14 @@
+player 1 = input("player 1, enter your choice (rock,paper,or scissors):")
+player 2 = input("player 2, enter your choice (rock,paper,or scissors):")
+if player 1 == player 2:
+   print("it's a lie")
+elif (player 1 == "rock" and player 2 == "scissors") or
+     (player 1 == "scissors" and player 2 == "pepar") or
+     (player 1 == "paper" and player 2 == "rock"):
+else:
+    print("player 1 wins"!)
+new_game = input("do you want to start a new game ? (yes/no) :")
+if new_game lower() == "yes":
+    print("starting a new game...")
+else :
+      print("thanks for playing !")
