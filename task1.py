d8c589c95cf34ecd6f2a8c14335ff434146fef13
Diya Mevada <diyamevada953@gmail.com>
2025-06-05 15:17:46 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,16 +1,16 @@
-1.addition/ 2.substraction/ 3.multiplication/ 4.division/ 5.modulo/
-n1 = enter the number one
-n2 = enter the number two
-choice = int(input(enter your choice))
+print("1.addition/ 2.substraction/ 3.multiplication/ 4.division/ 5.modulo/")
+choice = int(input("enter your choice"))
+n1 = int(input("enter the first number"))
+n2 = int(input("enter the second number"))
 if choice == 1 :
-   print("addition",:n1+n2)
+   print("addition",n1+n2)
 elif choice == 2 :
-   print("substraction",:n1-n2)
+   print("substraction",n1-n2)
 elif choice == 3 :
-   print("multiplication",:n1*n2)
+   print("multiplication",n1*n2)
 elif choice == 4 :
-   print("division",:n1/n2)
+   print("division",n1/n2)
 elif choice == 5 :
-   print("modulo",:n1%n2)
+   print("modulo",n1%n2)
 else :
-   print("default choice") 
+   print("invalid choice") 
