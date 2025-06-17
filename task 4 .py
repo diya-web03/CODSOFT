f0e30ba42e67a48a464694591d4f71be342ab3f5
Diya Mevada <diyamevada953@gmail.com>
2025-06-17 14:32:58 +0530

    Update task 4 .py

--- task 4 .py
+++ task 4 .py
@@ -1,14 +1,15 @@
-player 1 = input("player 1, enter your choice (rock,paper,or scissors):")
-player 2 = input("player 2, enter your choice (rock,paper,or scissors):")
+player1 = input("player 1, enter your choice (rock,paper,or scissors):").lower()
+player2 = input("player 2, enter your choice (rock,paper,or scissors):").lower()
 if player 1 == player 2:
-   print("it's a lie")
-elif (player 1 == "rock" and player 2 == "scissors") or
-     (player 1 == "scissors" and player 2 == "pepar") or
-     (player 1 == "paper" and player 2 == "rock"):
+   print("it's a tie!")
+elif (player1 == "rock" and player2 == "scissors") or \
+     (player1 == "scissors" and player2 == "paper") or \
+     (player1 == "paper" and player2 == "rock"):
+    print("player 1 wins!")
 else:
-    print("player 1 wins"!)
-new_game = input("do you want to start a new game ? (yes/no) :")
-if new_game lower() == "yes":
+   print("player 2 wins!")
+new_game = input("Do you want to start a new game ? (yes/no):")
+if new_game.lower() == "yes":
     print("starting a new game...")
 else :
       print("thanks for playing !")
